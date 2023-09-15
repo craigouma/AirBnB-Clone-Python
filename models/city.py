eda90352ce851bacf9bc9a32abc4a8f21fbb3667
Craig Carlos Ouma <craigcarlos95@gmail.com>
2023-09-15 18:50:00 +0300

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,1 +1,12 @@
+#!/usr/bin/python3
+
+""" module containing the City class """
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ class that acts as a blueprint for a City instance """
+    state_id = ""
+    name = ""
 
