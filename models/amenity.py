8c45be34e4db56ef1104ad899c0a7bf5ec50a836
Craig Carlos Ouma <craigcarlos95@gmail.com>
2023-09-15 18:48:34 +0300

    Update amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,11 @@
+#!/usr/bin/python3
+
+""" class that defines an Amenity class """
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """ Amenity class """
+    name = ""
 
