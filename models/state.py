9d305a88a8a0b9b0bc7dc645257edc288f5ca2b5
Craig Carlos Ouma <craigcarlos95@gmail.com>
2023-09-15 20:09:23 +0300

    Commit code

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" module to define a State class """
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ class that serves as a blueprint for State instances """
+    name = ""
